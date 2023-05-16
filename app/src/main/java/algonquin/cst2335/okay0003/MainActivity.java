0b19f74c3d8042ebd037054a0246d84e9548839d
Hakan Okay <hokay18@gmail.com>
2023-05-16 19:37:41 -0400

    Old way of adding widgets

--- app/src/main/java/algonquin/cst2335/okay0003/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/okay0003/MainActivity.java
@@ -2,6 +2,11 @@ package algonquin.cst2335.okay0003;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button mybutton = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+            }
+        });
     }
 }
